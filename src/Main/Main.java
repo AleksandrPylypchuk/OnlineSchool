4661f82705d7606d67de16c890c7ba95f9a9c168
Aleksandr <Pilipchuk_alex@ukr.net>
2023-02-20 21:25:12 +0200

    feature make conflict

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,6 +11,8 @@ public class Main {
         Courses CourseOne = new Courses("Lesson1");
         Courses CourseTwo = new Courses("Lesson2");
         Courses CoursesThree = new Courses("Lesson3");
+        Courses CoursesFour = new Courses("Lesson4");
+        Courses CoursesFive = new Courses("Lesson5");
         System.out.println(Courses.ID);
 
     }
